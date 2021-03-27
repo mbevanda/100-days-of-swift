d8baae0455fb08d13670795af593ccceebfe5592
Maja Bevada <majabevada@Majas-MacBook-Air.local>
2021-03-27 20:46:02 +0100

    Added project one with challenge completed

---
+++ 01StormViewer/01StormViewer/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  01StormViewer
+//
+//  Created by Maja Bevada on 26.03.2021..
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       // title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        navigationController?.hidesBarsOnTap = false
+    }
+}
