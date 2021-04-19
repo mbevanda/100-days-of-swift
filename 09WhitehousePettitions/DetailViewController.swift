a7777d4649d1b07269a441967b65ad524d8b0ef2
Maja Bevada <majabevada@Majas-MacBook-Air.local>
2021-04-19 21:41:01 +0200

    Minor change in petition details UI

--- 09WhitehousePettitions/DetailViewController.swift
+++ 09WhitehousePettitions/DetailViewController.swift
@@ -28,7 +28,7 @@ class DetailViewController: UIViewController {
         <html>
         <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style> body { font-size: 150%; } </style>
+        <style> body { font-size: 120%; font-family: Helvetica; } </style>
         </head>
         <body>
         \(detailItem.body)
