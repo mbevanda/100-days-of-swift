7cae5368f4f43cdac7eb098fdef4706390918b9d
Maja Bevada <majabevada@Majas-MacBook-Air.local>
2021-04-20 18:07:07 +0200

    Added score label

---
+++ 010SwiftyWords/010SwiftyWords/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  010SwiftyWords
+//
+//  Created by Maja Bevada on 20.04.2021..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var cluesLablel: UILabel!
+    var answerLabel: UILabel!
+    var currentAnswer: UITextField!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view = UIView()
+        view.backgroundColor = .white
+        
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+        
+        NSLayoutConstraint.activate([
+            scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+        ])
+        
+    }
+
+
+}
+
