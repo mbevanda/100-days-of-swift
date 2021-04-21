4e2d191c4715167196affed8cf0e3f13e1a0d4bf
Maja Bevada <majabevada@Majas-MacBook-Air.local>
2021-04-21 17:04:35 +0200

    Added submit and clear buttons

--- 010SwiftyWords/010SwiftyWords/ViewController.swift
+++ 010SwiftyWords/010SwiftyWords/ViewController.swift
@@ -50,6 +50,15 @@ class ViewController: UIViewController {
         currentAnswer.isUserInteractionEnabled = false
         view.addSubview(currentAnswer)
         
+        let submit = UIButton(type: .system)
+        submit.translatesAutoresizingMaskIntoConstraints = false
+        submit.setTitle("SUBMIT", for: .normal)
+        view.addSubview(submit)
+        
+        let clear = UIButton(type: .system)
+        clear.translatesAutoresizingMaskIntoConstraints = false
+        clear.setTitle("CLEAR", for: .normal)
+        view.addSubview(clear)
         
         NSLayoutConstraint.activate([
             scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
@@ -63,9 +72,13 @@ class ViewController: UIViewController {
             answerLabel.heightAnchor.constraint(equalTo: cluesLablel.heightAnchor),
             currentAnswer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             currentAnswer.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.5),
-            currentAnswer.topAnchor.constraint(equalTo: cluesLablel.bottomAnchor, constant: 20)
-            
-            
+            currentAnswer.topAnchor.constraint(equalTo: cluesLablel.bottomAnchor, constant: 20),
+            submit.topAnchor.constraint(equalTo: currentAnswer.bottomAnchor),
+            submit.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -100),
+            submit.heightAnchor.constraint(equalToConstant: 44),
+            clear.centerYAnchor.constraint(equalTo: submit.centerYAnchor),
+            clear.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 100),
+            clear.heightAnchor.constraint(equalToConstant: 44)
         ])
         
         // temporary for easier understanding
