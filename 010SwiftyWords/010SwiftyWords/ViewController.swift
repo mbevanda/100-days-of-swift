a0a2ec623dba1c45c63da18f5c2c63d52066f903
Maja Bevada <majabevada@Majas-MacBook-Air.local>
2021-04-21 16:14:05 +0200

    Added clues/answers labels

--- 010SwiftyWords/010SwiftyWords/ViewController.swift
+++ 010SwiftyWords/010SwiftyWords/ViewController.swift
@@ -27,11 +27,36 @@ class ViewController: UIViewController {
         scoreLabel.text = "Score: 0"
         view.addSubview(scoreLabel)
         
+        cluesLablel = UILabel()
+        cluesLablel.translatesAutoresizingMaskIntoConstraints = false
+        cluesLablel.font = UIFont.systemFont(ofSize: 24)
+        cluesLablel.text = "CLUES"
+        cluesLablel.numberOfLines = 0
+        view.addSubview(cluesLablel)
+        
+        answerLabel = UILabel()
+        answerLabel.translatesAutoresizingMaskIntoConstraints = false
+        answerLabel.font = UIFont.systemFont(ofSize: 24)
+        answerLabel.text = "ANSWER"
+        answerLabel.numberOfLines = 0
+        answerLabel.textAlignment = .right
+        view.addSubview(answerLabel)
+        
         NSLayoutConstraint.activate([
             scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
             scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            cluesLablel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor),
+            cluesLablel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 100),
+            cluesLablel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.6, constant: -100),
+            answerLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor),
+            answerLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -100),
+            answerLabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.4, constant: -100),
+            answerLabel.heightAnchor.constraint(equalTo: cluesLablel.heightAnchor)
         ])
         
+        // temporary for easier understanding
+        cluesLablel.backgroundColor = .red
+        answerLabel.backgroundColor = .blue
     }
 
 
