c6aa6cadf585a0a6c51e6efbd3876b417a907e50
Maja Bevada <majabevada@Majas-MacBook-Air.local>
2021-04-26 21:49:35 +0200

    Using previous project and modifing that all our loading code run in the background queue

---
+++ 011GrandCentralDispatch/DetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  DetailViewController.swift
+//  09WhitehousePettitions
+//
+//  Created by Maja Bevada on 18.04.2021..
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    var webView: WKWebView!
+    var detailItem: Petition?
+    
+    override func loadView() {
+        super.loadView()
+        
+        webView = WKWebView()
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let detailItem = detailItem else { return }
+
+        let html = """
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style> body { font-size: 120%; font-family: Helvetica; } </style>
+        </head>
+        <body>
+        \(detailItem.body)
+        </body>
+        </html>
+        """
+
+        webView.loadHTMLString(html, baseURL: nil)
+    }
+
+}
