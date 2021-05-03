05fbacaf4ffed2194e1806f2cc0ad0a1549393fc
Maja Bevada <majabevada@Majas-MacBook-Air.local>
2021-05-03 15:36:28 +0200

    Added data sources to view

--- 013NamesToFaces/013NamesToFaces/ViewController.swift
+++ 013NamesToFaces/013NamesToFaces/ViewController.swift
@@ -12,6 +12,18 @@ class ViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let  cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            //we failed to get a PersonCell - bail out!
+            fatalError("Unable to dequeue PersonCell.")
+        }
+        return cell
+    }
 
 
 }
