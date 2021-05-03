f8fd58109411a639d271a3b435d9592b1f141ccc
Maja Bevada <majabevada@Majas-MacBook-Air.local>
2021-05-03 14:19:24 +0200

    Initial commit and designing UICollectionView cell

---
+++ 013NamesToFaces/013NamesToFaces/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  013NamesToFaces
+//
+//  Created by Maja Bevada on 03.05.2021..
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
