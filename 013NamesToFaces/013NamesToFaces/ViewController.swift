6139b000ac6d01b54ed72d60ac6e91a12c2764e7
Maja Bevada <majabevada@Majas-MacBook-Air.local>
2021-05-04 16:40:35 +0200

    Added functionality for deleting pictures from grid

--- 013NamesToFaces/013NamesToFaces/ViewController.swift
+++ 013NamesToFaces/013NamesToFaces/ViewController.swift
@@ -45,6 +45,19 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let person = people[indexPath.item]
         
+        let ac = UIAlertController(title: "Choose action", message: "Do you want to delete person or rename it", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Delete" , style: .default ){ [weak self] _ in
+            self?.people.remove(at: indexPath.item)
+            self?.collectionView.deleteItems(at: [indexPath])
+        })
+        ac.addAction(UIAlertAction(title: "Rename" , style: .default){ [weak self] _ in
+            self?.renamePerson(person)
+        })
+        present(ac, animated: true)
+    }
+    
+    func renamePerson (_ person: Person) {
+        
         let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
         ac.addTextField()
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
