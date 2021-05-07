bffa9070b0cbd8a5d1ddc28dcc638be7723d4a68
Maja Bevada <majabevada@Majas-MacBook-Air.local>
2021-05-07 16:03:01 +0200

    Bouncers added

--- 014Pachinko/014Pachinko/GameScene.swift
+++ 014Pachinko/014Pachinko/GameScene.swift
@@ -15,6 +15,12 @@ class GameScene: SKScene {
         background.zPosition = -1
         addChild(background)
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        makeBouncer(at: CGPoint(x: 0, y: 0))
+        makeBouncer(at: CGPoint(x: 256, y: 0))
+        makeBouncer(at: CGPoint(x: 512, y: 0))
+        makeBouncer(at: CGPoint(x: 768, y: 0))
+        makeBouncer(at: CGPoint(x: 1024, y: 0))        
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -26,5 +32,13 @@ class GameScene: SKScene {
             ball.position = location
             addChild(ball)            
         }
+    }
+    
+    func makeBouncer (at position: CGPoint) {
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2.0)
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
     }
 }
