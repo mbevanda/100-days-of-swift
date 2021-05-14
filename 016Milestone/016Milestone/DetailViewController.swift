3a6dc4d7347bd3db85bc71df7af2fa429317c2cf
Maja Bevada <majabevada@Majas-MacBook-Air.local>
2021-05-14 15:33:56 +0200

    Creating UI for app

---
+++ 016Milestone/016Milestone/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  016Milestone
+//
+//  Created by Maja Bevada on 14.05.2021..
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var selectedImage: String!
+
+}
---
+++ 016Milestone/016Milestone/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  016Milestone
+//
+//  Created by Maja Bevada on 14.05.2021..
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var userPhotos:[String] = ["Unknown"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        userPhotos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = userPhotos[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vs = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
+            
+            navigationController?.pushViewController(vs, animated: true)
+        }
+    }
+}
+
